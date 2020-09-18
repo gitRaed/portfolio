87c425875d3f84be0d29c90ee4aa2d5a50ddcdb8
Raed <raedfaddoul@gmail.com>
2020-09-18 15:21:39 +0100

    Updates

---
+++ precache-manifest.70fb6acd4aec9cf5178373687a881fab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962bdfedde4ec30c2692f2affb7fd33d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a32f712e7beba60d34f6",
+    "url": "/portfolio/static/css/2.edeedb01.chunk.css"
+  },
+  {
+    "revision": "a113ece124e5ff2515b9",
+    "url": "/portfolio/static/css/main.6fcab306.chunk.css"
+  },
+  {
+    "revision": "a32f712e7beba60d34f6",
+    "url": "/portfolio/static/js/2.39bb92f7.chunk.js"
+  },
+  {
+    "revision": "a60c16ded7f4ceccde5b77ad2f56fc47",
+    "url": "/portfolio/static/js/2.39bb92f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a113ece124e5ff2515b9",
+    "url": "/portfolio/static/js/main.334fd7be.chunk.js"
+  },
+  {
+    "revision": "86ab21c6030aeb34ea52",
+    "url": "/portfolio/static/js/runtime-main.3ff7c761.js"
+  },
+  {
+    "revision": "424c3ddffb0baaf29875e1d8dafc8cdc",
+    "url": "/portfolio/static/media/cv.424c3ddf.pdf"
+  },
+  {
+    "revision": "6453d7fd553eaa6a6b3cc45884507acf",
+    "url": "/portfolio/static/media/fcc-banner.6453d7fd.jpg"
+  },
+  {
+    "revision": "cb77ad0a8d9995a14a7e45594e4f9967",
+    "url": "/portfolio/static/media/raed2.cb77ad0a.png"
+  }
+]);
