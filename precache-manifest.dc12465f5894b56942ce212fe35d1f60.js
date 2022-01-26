4bae377970fab8d152f7983d675db268a7c3da4f
raed <52961364+gitRaed@users.noreply.github.com>
2022-01-26 12:46:37 +0100

    Updates

---
+++ precache-manifest.dc12465f5894b56942ce212fe35d1f60.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebdf15bf5140f9ffb7e69130313b6a6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d859dde139b5165d9c9f",
+    "url": "/portfolio/static/css/2.edeedb01.chunk.css"
+  },
+  {
+    "revision": "63c8906e5251eda2a6a2",
+    "url": "/portfolio/static/css/main.7bf253cd.chunk.css"
+  },
+  {
+    "revision": "d859dde139b5165d9c9f",
+    "url": "/portfolio/static/js/2.cd3a031e.chunk.js"
+  },
+  {
+    "revision": "a60c16ded7f4ceccde5b77ad2f56fc47",
+    "url": "/portfolio/static/js/2.cd3a031e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c8906e5251eda2a6a2",
+    "url": "/portfolio/static/js/main.1b39e906.chunk.js"
+  },
+  {
+    "revision": "86ab21c6030aeb34ea52",
+    "url": "/portfolio/static/js/runtime-main.3ff7c761.js"
+  },
+  {
+    "revision": "b649ff587cc1b2ff6964139813c2d20c",
+    "url": "/portfolio/static/media/cv.b649ff58.pdf"
+  },
+  {
+    "revision": "6453d7fd553eaa6a6b3cc45884507acf",
+    "url": "/portfolio/static/media/fcc-banner.6453d7fd.jpg"
+  },
+  {
+    "revision": "dbfeac2b8690b202613d4b4c09082210",
+    "url": "/portfolio/static/media/moi.dbfeac2b.jpg"
+  },
+  {
+    "revision": "c5514c8788cc5e37ef0aed506ebb6a96",
+    "url": "/portfolio/static/media/yelp_logo.c5514c87.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a8cb8fe0f133b23741d3e844af29a8f8.js"
+  "/portfolio/precache-manifest.dc12465f5894b56942ce212fe35d1f60.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
